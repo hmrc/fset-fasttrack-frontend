d1ab0a30e36fc22b0c5d9bc42d1ebc554d57fdc9
Henri Cook <github@henricook.com>
2017-01-06 15:33:33 +0000

    Add permissions to address lookup endpoint

--- app/controllers/AddressLookupController.scala
+++ app/controllers/AddressLookupController.scala
@@ -19,12 +19,13 @@ package controllers
 import config.CSRHttp
 import connectors.addresslookup.AddressLookupClient
 import play.api.mvc.{ Action, AnyContent }
+import security.Roles.SchemesRole
 
 trait AddressLookupController extends BaseController {
   val addressLookupClient: AddressLookupClient
 
-  // TODO Add permissions into this once the feature is ready to be moved from test routes
-  def addressLookup(postcode: String): Action[AnyContent] = Action.async { implicit request =>
+  def addressLookup(postcode: String): Action[AnyContent] = CSRSecureAction(SchemesRole) {
+    implicit request => implicit cachedData =>
     val decoded = java.net.URLDecoder.decode(postcode, "UTF8")
     addressLookupClient.findByPostcode(decoded, None).map(r => Ok(r.toString))
   }
