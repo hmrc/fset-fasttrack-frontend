c0a607cba680f9b9dba4c60c1800bc8ec69239bf
Ian Stainer <ian.stainer@valtech.co.uk>
2017-01-17 16:31:31 +0000

    fset-1152 added ajax call for the schools lookup

---
+++ app/controllers/SchoolsController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.{ CSRCache, CSRHttp }
+import connectors.SchoolsClient
+import connectors.SchoolsClient.SchoolsNotFound
+import models.view.SchoolView
+import models.view.SchoolView.{ SchoolImplicits, _ }
+import play.api.libs.json.Json
+import security.Roles.EducationQuestionnaireRole
+
+import scala.concurrent.Future
+import scala.language.reflectiveCalls
+
+object SchoolsController extends SchoolsController {
+  val http = CSRHttp
+  val cacheClient = CSRCache
+}
+
+trait SchoolsController extends BaseController with SchoolsClient {
+  def getSchools(term: String) = CSRSecureAppAction(EducationQuestionnaireRole) { implicit request =>
+    implicit user =>
+      if (term.trim.nonEmpty) {
+        SchoolsClient.getSchools(term).map { schools =>
+          val schoolViewList = schools match {
+            case _ if schools.size > limitResults => narrowYourSearchHint +: schools.map(_.toSchoolView).take(limitResults)
+            case _ => schools.map(_.toSchoolView)
+          }
+          Ok(Json.toJson(schoolViewList))
+        }.recover {
+          case _: SchoolsNotFound => BadRequest("could not locate school list")
+        }
+      } else {
+        Future.successful(Ok(Json.toJson(List.empty[SchoolView])))
+      }
+  }
+}
