9aad0b8082b0aa5f73abe0e24017c69378da193d
Ben Stephenson <bjames.stephenson@gmail.com>
2017-02-01 13:52:08 +0000

    Complete test endpoint.

--- app/controllers/OnlineTestController.scala
+++ app/controllers/OnlineTestController.scala
@@ -17,7 +17,7 @@
 package controllers
 
 import config.{ CSRCache, CSRHttp }
-import connectors.ApplicationClient
+import connectors.OnlineTestClient
 import models.ApplicationData.ApplicationStatus
 import models.UniqueIdentifier
 import play.api.mvc.{ Action, AnyContent }
@@ -28,42 +28,20 @@ import scala.concurrent.Future
 object OnlineTestController extends OnlineTestController {
   val http = CSRHttp
   val cacheClient = CSRCache
+
+  val onlineTestClient = OnlineTestClient
 }
 
 trait OnlineTestController extends BaseController {
 
-  // TODO: I think the names can be improved
-  // startTests is plural but then we call getTestAssesment.
-  // It also seems we are using different names to refer to the same thing: "Tests", "onlineTest", "TestAssesment"
-  // I suggest rename startTests to start as we are in OnlineTestController
-  // getTestAssesment returns an Future[OnlineTestDetails], I think a more appropriate name would be
-  // getOnlineTestDetails or rename OnlineTestDetails to TestAssesment.
-  // then ".flatMap { onlineTest =>" should be consistent which whatever choice
-  // To sum up it could be like this:
-  /*
-  def start = CSRSecureAppAction(OnlineTestInvitedRole) { implicit request =>
-    implicit user =>
-      getOnlineTestDetails(user.user.userID).flatMap { onlineTestDetails =>
-        updateStatusOnlineTestDetails(user.user.userID, ApplicationStatus.ONLINE_TEST_STARTED).map { _ =>
-          Redirect(onlineTestDetails.getTestLink())
-        }
-      }
-  }
-
-  def complete(token: UniqueIdentifier) = CSRUserAwareAction { implicit request =>
-    implicit user =>
-      completeOnlineTest(token).map { _ =>
-        Ok(views.html.application.onlineTestSuccess())
-      }
-  }
-*/
+  def onlineTestClient: OnlineTestClient
 
   def startOrContinueTest(cubiksUserId: Int): Action[AnyContent] = CSRSecureAppAction(OnlineTestInvitedRole) { implicit request =>
     implicit user =>
-      getTestAssessment(user.user.userID).flatMap { onlineTest =>
+      onlineTestClient.getTestAssessment(user.user.userID).flatMap { onlineTest =>
 
         val maybeUpdateTestProfile = if (!onlineTest.isStarted) {
-          startOnlineTests(cubiksUserId)
+          onlineTestClient.startOnlineTests(cubiksUserId)
         } else {
          Future.successful(())
         }
@@ -76,17 +54,23 @@ trait OnlineTestController extends BaseController {
 
   def downloadPDFReport: Action[AnyContent] = CSRSecureAppAction(DisplayOnlineTestSectionRole) { implicit request =>
     implicit user =>
-      getPDFReport(user.application.applicationId).map { pdfBinary =>
+      onlineTestClient.getPDFReport(user.application.applicationId).map { pdfBinary =>
         Ok(pdfBinary).as("application/pdf")
           .withHeaders(("Content-Disposition", s"""attachment;filename="report-${user.application.applicationId}.pdf" """))
       }
   }
 
-  def complete(token: UniqueIdentifier): Action[AnyContent] = CSRUserAwareAction { implicit request =>
+  def completeByToken(token: UniqueIdentifier): Action[AnyContent] = CSRUserAwareAction { implicit request =>
     implicit user =>
-      completeTests(token).map { _ =>
+      onlineTestClient.completeTests(token).map { _ =>
         Ok(views.html.application.onlineTestSuccess())
       }
+  }
+
+  def completeTest(cubiksUserId: Int): Action[AnyContent] = CSRUserAwareAction { implicit request =>
+    implicit user =>
+      onlineTestClient.completeTests(
+
   }
 
 }
